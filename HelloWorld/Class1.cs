3c6127ec5137c95f7008a03f6b3f65b7ec9ba504
Ruslan Naumoff <ruslan4eg62@gmail.com>
2020-11-24 22:31:03 +0300

    Добавьте файлы проекта.

---
+++ HelloWorld/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Class1
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        
+        
+
+
+
+    }   
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Введите своё имя");
+            name = Console.ReadLine();
+            Console.Write("Здравствуйте, " + name);
+
+
+        }
+    }
+}
